6b2b20dd0dd3268401b9bec01ca218eafd323d2d
YorKnEz <alexmitreanu04@gmail.com>
2020-12-26 19:54:06 +0200

    init

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const server = require("http").createServer(app)
+const io = require("socket.io")(server)
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+  console.log('a user connected :D')
+  socket.on('chat message', msg => {
+    console.log(msg)
+    io.emit('chat message', msg)
+  })
+})
+
+server.listen(port, () => {
+  console.log("server running on port:" + port)
+})
